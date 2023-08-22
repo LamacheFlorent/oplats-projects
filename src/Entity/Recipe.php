42c65b404d06412d5b7a985c719553468079ea09
Benjamin Pelissier <benjpls@gmail.com>
2023-08-22 10:21:19 +0200

    ajout des relations des entitées

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\RecipeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,22 @@ class Recipe
      */
     private $codeAPI;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Review::class, mappedBy="recipe")
+     */
+    private $review;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Author::class, mappedBy="recipe")
+     */
+    private $authors;
+
+    public function __construct()
+    {
+        $this->review = new ArrayCollection();
+        $this->authors = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +53,63 @@ class Recipe
     public function setCodeAPI(int $codeAPI): self
     {
         $this->codeAPI = $codeAPI;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Review>
+     */
+    public function getReview(): Collection
+    {
+        return $this->review;
+    }
+
+    public function addReview(Review $review): self
+    {
+        if (!$this->review->contains($review)) {
+            $this->review[] = $review;
+            $review->setRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReview(Review $review): self
+    {
+        if ($this->review->removeElement($review)) {
+            // set the owning side to null (unless already changed)
+            if ($review->getRecipe() === $this) {
+                $review->setRecipe(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Author>
+     */
+    public function getAuthors(): Collection
+    {
+        return $this->authors;
+    }
+
+    public function addAuthor(Author $author): self
+    {
+        if (!$this->authors->contains($author)) {
+            $this->authors[] = $author;
+            $author->addRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuthor(Author $author): self
+    {
+        if ($this->authors->removeElement($author)) {
+            $author->removeRecipe($this);
+        }
 
         return $this;
     }
