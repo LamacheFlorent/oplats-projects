3ab5ea501e913ae6821da405f9805502d052258c
Florent Lamache <florent.lamache@oclock.school>
2023-08-23 21:20:00 +0200

    fixture rating

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,44 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Review;
+use Faker\Factory;
+use Doctrine\DBAL\Connection;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+    private function truncate()
+    {
+
+        $this->connection->executeQuery('SET foreign_key_checks = 0');
+
+        $this->connection->executeQuery('TRUNCATE TABLE review');
+
+        $this->connection->executeQuery('SET foreign_key_checks = 1');
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->truncate();
+
+        $faker =Factory::create();
+
+        $reviewsList = [];
+
+            $reviews = new Review();
+
+            $reviews->setRate($faker->randomFloat(1, 1, 5));
+    
+            $reviewsList[] = $reviews;
+
+            $manager->persist($reviews);
 
         $manager->flush();
     }
