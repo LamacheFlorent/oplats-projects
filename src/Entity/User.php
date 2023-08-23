c569daa26553d412743a0db3a152c0ff4dd58f2e
Florent Lamache <florent.lamache@oclock.school>
2023-08-23 13:30:30 +0200

    update registerController registrationFormType User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $review;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $username;
 
